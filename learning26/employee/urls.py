e22e1c5f985cca6425d21b38bbaa5e65aedf4f7c
samirvithlani <samir.vithlani83955@gmail.com>
2026-02-11 11:10:57 +0530

    feat: Implement Course model and add ModelForm for Employee and Course creation with associated views, URLs, and templates.

--- learning26/employee/urls.py
+++ learning26/employee/urls.py
@@ -4,4 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('employeeList/', views.employeeList),
     path('employeeFilter/', views.employeeFilter),
+    path('createemployee/',views.createEmployee),
+    path('createEmployeeWithForm/',views.createEmployeeWithForm),
+    path('createCourse/',views.createCourse)
 ]
