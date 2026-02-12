31ee94fefee1e42da69c2f1551f518849f53f37a
samirvithlani <samir.vithlani83955@gmail.com>
2026-02-12 10:57:18 +0530

    feat: Add employee management features including listing, creation, filtering, and deletion with new views, templates, and URL configurations.

--- learning26/employee/urls.py
+++ learning26/employee/urls.py
@@ -2,9 +2,13 @@
 from . import views
 from django.urls import path
 urlpatterns = [
-    path('employeeList/', views.employeeList),
+    path('employeeList/', views.employeeList,name="employeeList"),
     path('employeeFilter/', views.employeeFilter),
     path('createemployee/',views.createEmployee),
-    path('createEmployeeWithForm/',views.createEmployeeWithForm),
-    path('createCourse/',views.createCourse)
+    path('createEmployeeWithForm/',views.createEmployeeWithForm,name="createEmployeeWithForm"),
+    path('createCourse/',views.createCourse),
+    #path('deleteEmployee/',views.deleteEmployee,name="deleteEmployee")
+    path("deleteEmployee/<int:id>",views.deleteEmployee,name="deleteEmployee"),
+    path("filterEmployee/",views.filterEmployee,name="filterEmployee")
+    
 ]
